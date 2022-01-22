4d4918fc9d54300e18cf798895655cc884872ea6
Renzon Caceres <renzon.caceres@ceiba.com.co>
2022-01-22 10:38:43 -0500

    Fix domain layer.

--- database/src/main/java/com/example/database/dao/PlaceDAO.kt
+++ database/src/main/java/com/example/database/dao/PlaceDAO.kt
@@ -4,9 +4,6 @@ import androidx.room.*
 import com.example.database.entities.PlaceEntity
 import com.example.database.entities.PlaceWithVehicleEntity
 import com.example.domain.enum.State
-import io.reactivex.Completable
-import io.reactivex.Maybe
-import io.reactivex.Single
 import java.util.*
 
 @Dao
@@ -14,18 +11,18 @@ interface PlaceDAO {
 
     @Transaction
     @Query("SELECT * FROM placeEntity")
-    fun getPlacesAll(): Single<List<PlaceWithVehicleEntity>>
+    fun getPlacesAll(): List<PlaceWithVehicleEntity>
 
     @Transaction
     @Query("SELECT * FROM placeEntity WHERE plac_state =:state")
-    fun getPlacesAllByState(state: State): Single<List<PlaceWithVehicleEntity>>
+    fun getPlacesAllByState(state: State): List<PlaceWithVehicleEntity>
 
     @Query("SELECT * FROM placeEntity WHERE plac_id = :id")
-    fun getPlaceById(id: Long) : Maybe<PlaceWithVehicleEntity>
+    fun getPlaceById(id: Long) : PlaceWithVehicleEntity
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun savePlace(place: PlaceEntity): Completable
+    fun savePlace(place: PlaceEntity): Boolean
 
     @Query("UPDATE placeEntity SET plac_free_date =:freeDate, plac_state =:state WHERE plac_id =:id")
-    fun updatePlace(id: Long, freeDate: Date, state: State): Completable
+    fun updatePlace(id: Long, freeDate: Date, state: State): Boolean
 }
--- database/src/main/java/com/example/database/dao/VehicleDAO.kt
+++ database/src/main/java/com/example/database/dao/VehicleDAO.kt
@@ -2,20 +2,17 @@ package com.example.database.dao
 
 import androidx.room.*
 import com.example.database.entities.VehicleEntity
-import io.reactivex.Completable
-import io.reactivex.Maybe
-import io.reactivex.Single
 
 @Dao
 interface VehicleDAO {
 
     @Transaction
     @Query("SELECT * FROM vehicleEntity")
-    fun getVehiclesAll(): Single<List<VehicleEntity>>
+    fun getVehiclesAll(): List<VehicleEntity>
 
     @Query("SELECT * FROM vehicleEntity WHERE vehi_id = :id")
-    fun getVehicleById(id: Long) : Maybe<VehicleEntity>
+    fun getVehicleById(id: Long) : VehicleEntity
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun saveVehicle(vehicleEntity: VehicleEntity): Completable
+    fun saveVehicle(vehicleEntity: VehicleEntity): Boolean
 }
