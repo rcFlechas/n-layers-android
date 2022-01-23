ea5691974f0c9b067b7e40a850ace42d273abaef
Renzon Caceres <renzon.caceres@ceiba.com.co>
2022-01-22 20:14:15 -0500

    Creating structure of presentarion module.

--- presentation/src/main/java/com/example/adnceiba/ui/activities/MainActivity.kt
+++ presentation/src/main/java/com/example/adnceiba/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.adnceiba
+package com.example.adnceiba.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.adnceiba.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
