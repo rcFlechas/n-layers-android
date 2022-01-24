82b1ab0a8d00bd2e73933ca6558d88010daea7a4
Renzon Caceres <renzon.caceres@ceiba.com.co>
2022-01-24 11:30:11 -0500

    Implement desing view.

--- presentation/src/main/java/com/example/adnceiba/ui/activities/MainActivity.kt
+++ presentation/src/main/java/com/example/adnceiba/ui/activities/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.adnceiba.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.adnceiba.R
+import com.example.adnceiba.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        configNav()
+    }
+    private fun configNav() {
+        NavigationUI.setupWithNavController(binding.bottomNavigation, Navigation.findNavController(this, R.id.nav_host_fragment))
     }
 }
