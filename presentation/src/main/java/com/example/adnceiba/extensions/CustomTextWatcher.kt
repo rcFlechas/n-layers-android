bc163bf2f8c8f6913c2abdb18e2e20ec859ede2b
Renzon Caceres <renzon.caceres@ceiba.com.co>
2022-01-31 19:32:38 -0500

    Fix code smell.

--- presentation/src/main/java/com/example/adnceiba/extensions/CustomTextWatcher.kt
+++ presentation/src/main/java/com/example/adnceiba/extensions/CustomTextWatcher.kt
@@ -6,11 +6,14 @@ import android.text.TextWatcher
 abstract class CustomTextWatcher : TextWatcher {
 
     override fun afterTextChanged(s: Editable?) {
+        s.toString()
     }
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        s.toString()
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        s.toString()
     }
 }
